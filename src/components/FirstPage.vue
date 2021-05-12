541a5b8a53ce0f49bc85f0c4d12e95a3ce3d20c3
jonagusth <jonagusth@gmail.com>
2021-05-12 16:07:23 +0200

    added carousel on second page

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="first">
-    <div class="row">
-      <div class="col-6">
+    <!-- <div class="row">
+      <div class="col-12">
         <div class="card">
-          <!-- <h5 class="card-header">Introduction</h5> -->
           <div class="card-body">
             <p>
               In this project we look and analize car ownership for Copenhagen
@@ -16,37 +15,56 @@
               plots and maps, and we analyzed it looking for trends and
               singularities. Our visualization focuses on:
             </p>
+            <ul class="list-group-flush">
+              <li class="list-group-item">
+                Car ownership by type over time in Copenhagen
+              </li>
+              <li class="list-group-item">
+                Car ownership type by neighborhood
+              </li>
+              <li class="list-group-item">
+                Trends of electric and hybrid cars, popularly knows as
+                <strong>'green cars'</strong>
+              </li>
+              <li class="list-group-item">
+                The future of car ownership in Copenhagen
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-      <div class="col-6">
-        <div class="card">
-          <div class="card-body">
-            <p>insert some graph over here</p>
-          </div>
-        </div>
+    </div> -->
+    <div class="row">
+      <div class="col-8">
+        <p>
+          In this project we look and analize car ownership for Copenhagen
+          metropolitan area in the period of time between 2006 and 2020. We
+          loaded and filtered the data from
+          <a href="https://www.dst.dk/">Statistik Banken</a>. The raw data
+          include tables on new private cars registered every year in every
+          neighborhood in Copenhagen. Then we plotted the data using different
+          visualization tools that include histograms, scatter plots and maps,
+          and we analyzed it looking for trends and singularities. Our
+          visualization focuses on:
+        </p>
+        <ul class="list-group-flush">
+          <li class="list-group-item border-0">Car ownership by type over time in Copenhagen</li>
+          <li class="list-group-item border-0">Car ownership type by neighborhood</li>
+          <li class="list-group-item border-0">
+            Trends of electric and hybrid cars, popularly knows as  <strong>'green cars'</strong>
+          </li>
+          <li class="list-group-item border-0">The future of car ownership in Copenhagen</li>
+        </ul>
+      </div>
+      <div class="col-4">
+        <img src="../assets/CopenhagenMap.gif" />
       </div>
     </div>
     <div class="row">
-      <div class="col-6"></div>
       <div class="col-6">
-        <div class="card">
-          <h5 class="card-header">Introduction</h5>
-          <div class="card-body">
-            <p>
-              In this project we look and analize car ownership for Copenhagen
-              metropolitan area in the period of time between 2006 and 2020. We
-              loaded and filtered the data from
-              <a href="https://www.dst.dk/">Statistik Banken</a>. The raw data
-              include tables on new private car registered every year in every
-              neighborhood in Copenhagen. Then we plotted the data using
-              different visualization tools that include histograms, scatter
-              plots and maps, and we analyzed it looking for trends and
-              singularities. Our visualization focuses on:
-            </p>
-          </div>
-        </div>
+        <img src="../assets/CopenhagenMap.gif" />
       </div>
+      <div class="col-6"></div>
     </div>
   </div>
 </template>
@@ -60,8 +78,13 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #first {
-  background-color: rgb(235, 230, 226);
+  /* background-color: rgb(235, 230, 226); */
+  background-color: rgb(255, 255, 255);
   height: 700px;
   width: 100%;
+}
+.li .borderless{
+  border-top: 0 none;
+  border-bottom: 0 none;
 }
 </style>
--- src/components/SecondPage.vue
+++ src/components/SecondPage.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="second">
-    <p>
-        Second Page
-    </p>
-    <p>
-        This will include whatever we want to have on the second page!
-    </p>
+    <p>Second Page</p>
+    <p>This will include whatever we want to have on the second page!</p>
+    <div class="row">
+      <div class="col-6">
+        Description about all the graphs regarding histograms in each
+        neighborhood
+      </div>
+      <div class="col-6">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
   </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../assets/Neighborhoods/AmagerØst.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/Neighborhoods/AmagerVest.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/Neighborhoods/Bispebjerg.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/Neighborhoods/Brønshøj.png" class="d-block w-100" >
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/Neighborhoods/Valby.png" class="d-block w-100" >
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/Neighborhoods/Vanløse.png" class="d-block w-100" >
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/Neighborhoods/Vesterbro.png" class="d-block w-100" >
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+  </div>
+  </div>
+
 </template>
 
 <script>
-
-
-
 export default {
-  name: 'SecondPage'
-}
+  name: "SecondPage",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #second {
-    background-color: rgb(235, 230, 226);
-    height: 700px;
-    width: 100%;
+  background-color: rgb(235, 230, 226);
+  height: 700px;
+  width: 100%;
 }
 </style>
