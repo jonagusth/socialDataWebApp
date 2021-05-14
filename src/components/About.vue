99b8dfdc0c15631b7961158fd892ee5e750e9e51
jonagusth <jonagusth@gmail.com>
2021-05-14 14:49:41 +0200

    laga about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="about">
-    <p>About US... kind of...</p>
     <div class="row">
       <div class="col-4">
         <img
@@ -48,7 +47,8 @@ export default {
 <style scoped>
 #about {
   background-color: rgb(235, 230, 226);
-  height: 700px;
+  /* height: 700px; */
   width: 100%;
+  height: 100%;
 }
 </style>
